77604db535abf1b40c7e96a36005d73da9c93d97
Genaro <genarorafault1@gmail.com>
2023-03-09 11:48:43 -0300

    prueba commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+const path = require('path');
+
+app.listen(3000, ()=>{
+    console.log('Servidor funcionando');
+    });
+
+const pathPublic = path.resolve(__dirname, 'public');
+const static = express.static(pathPublic);
+app.use(static);
+
+
+app.get("/carro", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/carrito.html"));
+});
+
+app.get("/acceso", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/acceso.html"));
+});
+
+app.get("/registro", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/registro.html"));
+});
+

0b2137a8fc6c8862c795b6ab8259fd807ca4a726
nahhhhir <nahirihan513@gmail.com>
2023-03-17 00:00:14 -0300

    intalado ejs y creado carpetas de estructura

--- app.js
+++ app.js
@@ -1,38 +1,38 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = 3000;
-const funca = 'Servidor funcionando en localhost: ';
+const port = process.env.PORT || 3000;
+const funca = () => console.log('Servidor funcionando en localhost: ');
 
-app.listen(port, ()=>{
-    console.log(funca + port);
-    });
+app.listen(port, funca);
+app.set("view engine", `ejs`);
+//const pathPublic = path.resolve(__dirname, 'public');
+//const static = express.static(pathPublic);
+//app.use(static);
 
-const pathPublic = path.resolve(__dirname, 'public');
-const static = express.static(pathPublic);
-app.use(static);
+app.use(express.static("./public"));
 
 
 app.get("/carro", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/carrito.html"));
+    res.sendFile(path.resolve(__dirname, "./src/views/carrito.html"));
 });
 
 app.get("/acceso", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/acceso.html"));
+    res.sendFile(path.resolve(__dirname, "./src/views/acceso.html"));
 });
 
 app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/home.html"));
+    res.sendFile(path.resolve(__dirname, "./src/views/home.html"));
 });
 
-app.get("/Producto", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/detalleProducto.html"));
+app.get("/producto", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./src/views/detalleProducto.html"));
 });
 
 app.get("/categoria-jeans", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/categoria-jeans.html"));
+    res.sendFile(path.resolve(__dirname, "./src/views/categoria-jeans.html"));
 });
 
 app.get("/detalleProducto", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/detalleProducto.html"));
+    res.sendFile(path.resolve(__dirname, "./src/views/detalleProducto.html"));
 });

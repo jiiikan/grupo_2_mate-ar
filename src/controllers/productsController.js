05cafacbc320195b7bafb7b688a6caf247547ff6
nahhhhir <nahirihan513@gmail.com>
2023-03-30 11:41:41 -0300

    crud parte1

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
+
+let products = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../data/products.json")));
 
 const productsController = {
 
     catalogo: (req, res) => {
-    res.render("products/catalogo")
+    res.render("products/catalogo", {products: products})
 },
     detalle: (req, res) => {
     res.render("products/detalleProducto")

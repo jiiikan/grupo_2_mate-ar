93bb6e3b02e8e6495cf7fc24acdfb5c7bb860faf
DanteStigliani <dantestigliani05@gmail.com>
2023-03-30 12:16:35 -0300

    app.js

--- app.js
+++ app.js
@@ -4,36 +4,46 @@ const path = require('path');
 const port = process.env.PORT || 3000;
 const funca = () => console.log('Servidor funcionando en localhost: ');
 
-const products = require("./src/routes/products.js");
-const users = require("./src/routes/users.js");
 
-app.listen(port, funca);
+// Config Ejs
+
+app.engine('html', require('ejs').renderFile);
+app.set("views", path.resolve(__dirname,"./views"));
 app.set("view engine", `ejs`);
 
-app.use(express.static("./public"));
+// Levantamos servidor   
+//- correr con node: npm start -- correr con nodemon: npm run dev -
 
-app.use("/", products);
-app.use("/catalogo", products);
+app.listen(port, funca);
+
+// Archivos estaticos 
+const public = path.resolve(__dirname, '../public');
+app.use(express.static(public));
+//app.use(express.static("./public"));
+
+// Rutas
+
+const home = require("./routes/home");
+const products = require("./routes/products.js");
+const users = require("./routes/users.js");
+
+//  Paginas 
+
+app.use("/", home)
+app.use("/products", products);
+app.use("/users", users);
+
+// Error 404
+app.use((req, res, next) => {
+    res.status(404).render("error404");
+    next();
+});
+
+/*app.use("/catalogo", products);
 app.use("/detalle", products);
-app.use("/iditor", products);
+app.use("/editor", products);
+app.use("/create", products);*/
 
-app.use("/login", users);
+/*app.use("/login", users);
 app.use("/registro", users);
-app.use("/carro", users);
-
-
-//app.get("/carro", (req, res) => {
-//    res.sendFile(path.resolve(__dirname, "./src/views/users/carrito.ejs"));
-//});
-//app.get("/login", (req, res) => {
-//    res.sendFile(path.resolve(__dirname, "./src/views/users/login.html"));
-//});
-//app.get("/registro", (req, res) => {
-//    res.sendFile(path.resolve(__dirname, "./src/views/users/registro.html"));
-//});
-
-
-
-//const pathPublic = path.resolve(__dirname, 'public');
-//const static = express.static(pathPublic);
-//app.use(static);
+app.use("/carrito", users);*/

bdd1b82e0db84057332b0835c8e3a536d45d2d8c
Genaro <genarorafault1@gmail.com>
2023-04-03 19:42:35 -0300

    store

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -10,14 +10,31 @@ const productsController = {
     res.render("products/catalogo", {products: products})
 },
     detalle: (req, res) => {
-    res.render("products/detalleProducto")
+    const productId = req.query.id;
+    const product = products.find((p) => p.id == productId);
+    res.render("products/detalleProducto", { product });
 },
     edition: (req, res) => {
     res.render("products/editionProducts")
 },
     create: (req, res) => {
-    res.render("products/createProducts")
+    res.render("products/createProducts")   
+},
+    store: (req, res) => {
+    const { product, description, price, image, category } = req.body;
+    const newProduct = {
+        id: products.length + 1,
+        nombre: product,
+        descripcion: description,
+        precio: presint(price),
+        imagen: image,
+        categoria: category
+    };
+    products.push(newProduct);
+    fs.writeFileSync(path.resolve(__dirname, "../data/products.json"), JSON.stringify(products, null, 2), "utf-8");
+    res.redirect("/catalogo");
 }
+
 };
 
 
--- src/routes/products.js
+++ src/routes/products.js
@@ -4,6 +4,7 @@ const productsController = require("../controllers/productsController.js");
 
 router.get("/catalogo", productsController.catalogo);
 router.get("/create", productsController.create);
+router.post("/create", productsController.store);
 router.get("/detalle", productsController.detalle);
 router.get("/edition", productsController.edition);
 

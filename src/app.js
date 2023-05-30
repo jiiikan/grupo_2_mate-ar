d5bbac0d9d849d28f8da2450c36ff7d10d4de1e4
Genaro <genarorafault1@gmail.com>
2023-05-30 18:21:24 -0300

    app

--- src/app.js
+++ src/app.js
@@ -3,24 +3,25 @@ const app = express();
 const path = require('path');
 const session = require('express-session');
 const cookies = require('cookie-parser');
-const port = process.env.PORT || 3000;
-const funca = () => console.log('Servidor funcionando en localhost: ');
 const bodyParser = require('body-parser');
 
-const userLoggedMiddleware = require('./middlewares/userLoggedMiddleware')
-//const methodOverride = require('method-override');
-// Config Ejs
+// CONFIGURACION DEL SERVIDOR
+const port = process.env.PORT || 3000;
+const funca = () => console.log('Servidor funcionando en localhost: ' + port);
 
+//Middleware para recordar usuario
+const userLoggedMiddleware = require('./middlewares/userLoggedMiddleware')
+
+// Config Ejs
 app.engine('html', require('ejs').renderFile);
 app.set("views", path.resolve(__dirname,"./views"));
 app.set("view engine", `ejs`);
 
 // Levantamos servidor   
 //- correr con node: npm start -- correr con nodemon: npm run dev -
-
 app.listen(port, funca);
 
-// Archivos estaticos 
+// Archivos estaticos (NECESARIOS) 
 const public = path.resolve(__dirname, '../public');
 app.use(express.static(public));
 app.use(express.json());
@@ -32,18 +33,14 @@ app.use(cookies());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.urlencoded({ extended: false}));
 
-//app.use(methodOverride("_method"))
-
 // Rutas
-
 const home = require("./routes/home");
 const products = require("./routes/products.js");
 const users = require("./routes/users.js");
 const { cookie } = require('express-validator');
 const api = require("./routes/api.js")
 
-//  Paginas 
-
+// Renderizacion de paginas 
 app.use("/", home)
 app.use("/products", products);
 app.use("/users", users);

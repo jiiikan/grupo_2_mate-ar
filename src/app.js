2907ce46039631651338df46abb8ab98bf4c14e8
Genaro <genarorafault1@gmail.com>
2023-06-15 22:39:58 -0300

    rutas

--- src/app.js
+++ src/app.js
@@ -50,7 +50,7 @@ const apiUsers = require('./routes/apiUsers')
 app.use("/", home)
 app.use("/products", products);
 app.use("/users", users);
-app.use("/api/", api);
+app.use("/api", api);
 app.use('/api/', apiUsers)
 
 // Error 404

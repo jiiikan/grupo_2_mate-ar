0a37a4c66c2951647adbbace281e95f2d2c037f6
Genaro <genarorafault1@gmail.com>
2023-06-16 18:25:01 -0300

    countByCategory terminado

--- src/controllers/apiProducts.js
+++ src/controllers/apiProducts.js
@@ -1,23 +1,44 @@
+const { name } = require('ejs');
 const db = require('../database/models');
 const op = db.sequelize.op;
 
 module.exports = {
     product: (req, res) =>{
-        db.Producto.findAll()
+        db.Producto.findAll({include: [{ 
+            association: "categories",
+        }]})
             .then(productos =>{
+                productsByCategory = {}
+                productos.forEach(product => {
+                    const categories = product.categories; // Obtener las categorías del producto actual
+                    if (Array.isArray(categories)) {
+                        categories.forEach(category => {
+                            const categoryName = category.name;
+                            if (!productsByCategory[categoryName]) {
+                                productsByCategory[categoryName] = 1;
+                            } else {
+                                productsByCategory[categoryName]++;
+                            }
+                        });
+                    } else {
+                        const categoryName = categories.name;
+                        if (!productsByCategory[categoryName]) {
+                            productsByCategory[categoryName] = 1;
+                        } else {
+                            productsByCategory[categoryName]++;
+                        }
+                    }
+                });
+                      
                 return res.status(200).json({
                     count:productos.length,
                     status: 200,
-                    countByCategory: db.Categoria.findAll()
-                        .then(categoria =>{
-                            return res.json({
-                                data: categoria
-                            })
-                        }),
+                    countByCategory: productsByCategory,
                     products: productos.map(product => ({
                         id: product.id,
                         name: product.name,
                         description: product.description,
+                        //categories: product.categories.map(category => category.name),
                         detail: `http://localhost:3000/api/products/${product.id}`
                     }))
                 })

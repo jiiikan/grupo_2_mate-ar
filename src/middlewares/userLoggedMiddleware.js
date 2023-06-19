01f51e0411de20c8a513796a131883bef1fd17a9
nahhhhir <nahirihan513@gmail.com>
2023-06-19 00:05:29 -0300

    css y comienzo filtro

--- src/middlewares/userLoggedMiddleware.js
+++ src/middlewares/userLoggedMiddleware.js
@@ -3,33 +3,33 @@ const db = require('../database/models');
 function userLoggedMiddleware(req, res, next) {
 	res.locals.isLogged = false;
 
-let emailInCookie = req.cookies.remember;
-	
-if (emailInCookie){
-        db.Usuario.findOne({
-	    where: {email: emailInCookie}
-	})
-	.then(userFromCookie => {
-		if (userFromCookie) {
-			req.session.userLogged = userFromCookie;
-		}
-		
-		if (req.session.userLogged) {
-		res.locals.isLogged = true;
-		res.locals.userLogged = req.session.userLogged;
-		}else{
-          res.clearCookie("remember");
-		}
- 
-	next();
-	})
-	.catch(error => {
-		console.log("Error al buscar el usuario", error)
+	let emailInCookie = req.cookies.remember;
+
+	if (emailInCookie) {
+		db.Usuario.findOne({
+			where: { email: emailInCookie }
+		})
+			.then(userFromCookie => {
+				if (userFromCookie) {
+					req.session.userLogged = userFromCookie;
+				}
+
+				if (req.session.userLogged) {
+					res.locals.isLogged = true;
+					res.locals.userLogged = req.session.userLogged;
+				} else {
+					res.clearCookie("remember");
+				}
+
+				next();
+			})
+			.catch(error => {
+				console.log("Error al buscar el usuario", error)
+				next();
+			});
+	} else {
 		next();
-	});
-	}else{
-		next();
-}
+	}
 }
 
 module.exports = userLoggedMiddleware;
